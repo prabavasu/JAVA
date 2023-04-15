efba546587f9d5ef24497474a4d4357b0a81234c
PrabakaranRengasamy <prabakarrengasamy10@gmail.com>
2023-04-15 07:20:02 +0530

    commit

---
+++ School.java
@@ -0,0 +1,19 @@
+class School
+{
+public static void main(String []args)
+{
+ 
+School navina = new School();
+navina.test(80,85,87,97,89);
+}
+void test(int Tamil,int English,int Maths,int Science,int Social)
+{
+System.out.println(Tamil);
+System.out.println(English);
+System.out.println(Maths);
+System.out.println(Science);
+System.out.println(Social);
+
+
+}
+}
