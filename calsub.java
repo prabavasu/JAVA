b3b814e76ef6a9f6dcd144ce494c5cbe20f5b6fd
PrabakaranRengasamy <prabakarrengasamy10@gmail.com>
2023-04-15 07:35:58 +0530

    first commit

---
+++ calsub.java
@@ -0,0 +1,17 @@
+class calsub
+{
+public static void main(String args[])
+{
+
+Calsub calc =new Calsub();
+
+calc.sub(); // method cal;ling statement            
+}
+
+void sub()  // method body / method definition -
+{
+System.out.println(20-10);
+}
+}
+
+
